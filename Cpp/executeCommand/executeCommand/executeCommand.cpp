93300c950c611026474bbc512567bdcdbd413ea2
SISUBENY_ <100661067+SISUBEN@users.noreply.github.com>
2023-10-10 23:59:19 +0800

    Add files via upload

---
+++ Cpp/executeCommand/executeCommand/executeCommand.cpp
@@ -0,0 +1,13 @@
+﻿// executeCommand.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+#include <Windows.h>
+#include <iostream>
+
+int main()
+{
+    std::cout << "executing...";
+    system("ping 8.8.8.8 -n 5");
+    system("curl ifconfig.io/ip");
+    system("pause");
+    return 0;
+}
